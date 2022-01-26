f5685417bb20b06ae840117570b0403ca7c073da
Pilgerin <Sobaka11>
2022-01-25 17:50:22 -0800

    converts images in  folder, changing their size and format

--- changeImage.py
+++ changeImage.py
@@ -5,13 +5,8 @@ from PIL import Image
 path = os.getcwd()
 path_in = path + '/supplier-data/images/'
 
-# if not os.path.exists(path_out):
-#   os.makedirs(path_out)
-# if new folder doesn't exist, create it
-
-
 for infile in os.listdir(path_in):
-    if not infile.startswith('.'):
+    if not infile.startswith('.') and 'tiff' in infile:
         im = Image.open(path_in + infile)
         im.convert('RGB').resize((600, 400)).save(path_in + infile, 'jpeg')
         im.close()
