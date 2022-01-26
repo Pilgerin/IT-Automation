5b28ec7f750074652ec86b3bdfc5dcd814064b9f
Pilgerin <Sobaka11>
2022-01-25 17:11:30 -0800

    adds files and descriptions into a json file

---
+++ run.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+import os
+import requests
+import json
+
+path_in = os.listdir('/supplier-data/descriptions')
+url = 'http://[linux-instance-IP-Address]/media/fruits/'
+for fdbck in path_in:
+    # traverses each file in the directory
+    fields = ['name', 'weight', 'description', 'image name']
+    if not fdbck.startswith('.'):
+        with open("/supplier-data/descriptions" + fdbck) as fb:
+            description = fb.read().split('\n')
+            print(description)
+            line_dict = {'name': description[0], 'weight': int(description[1].strip('lbs')),
+                         'description': description[2],
+                         'image name': os.path.basename('/supplier-data/images'+fdbck.strip('txt')+'jpeg')}
+            r = requests.post(url, data=line_dict)
+        with open('report.txt', 'w') as convert_file:
+            convert_file.write(json.dumps(line_dict))
+
+print(r.text)
+print(r.status_code)
