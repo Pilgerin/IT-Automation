524b9d5fa07bf0979a5b2da785ee2af8140b17f7
Pilgerin <Sobaka11>
2022-01-25 18:47:19 -0800

    Upload multiple images from specified directory to the server

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -2,17 +2,13 @@
 import os
 import requests
 
-from multiprocessing import Pool
-
 path_in = os.listdir('/supplier-data/images')
-#Django needs trailing slash!
 url = 'http://[linux-instance-IP-Address]/media/images/'
-multiple_files = []
+# Django needs trailing slash!
 for img in path_in:
     # traverses each file in the directory
-    if not img.startswith('.'):
-        with open("/supplier-data/images" + img) as fb:
-            multiple_files.extend(('images', ('img.jpg', open(img, 'rb'), 'image/png')))
-            r = requests.post(url, data=multiple_files)
+    if not img.startswith('.') and "jpeg" in img:
+        with open(path_in + img, 'rb') as fb:
+            r = requests.post(url, files={'file': fb})
 print(r.text)
 print(r.status_code)
