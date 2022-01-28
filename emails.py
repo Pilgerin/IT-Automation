6c47fcdbe658b7fbe8c39eca25e09ef4c04b2fb0
Pilgerin <Sobaka11>
2022-01-28 11:23:20 -0800

    Generates and sends a generic email (by default, without an attachment) with an option to include an attachment

--- emails.py
+++ emails.py
@@ -6,7 +6,7 @@ import os.path
 import smtplib
 
 
-def generate(sender, recipient, subject, body, attachment_path):
+def generate(sender, recipient, subject, body, attachment_path = None):
     """Creates an email with an attachment."""
     # Basic Email formatting
     message = email.message.EmailMessage()
@@ -16,14 +16,14 @@ def generate(sender, recipient, subject, body, attachment_path):
     message.set_content(body)
 
     # attachment needs to be optional to take care of health check email without attachment
-    if not attachment_path == "":
+    if attachment_path is not None:
         # Process the attachment and add it to the email
         attachment_filename = os.path.basename(attachment_path)
         mime_type, _ = mimetypes.guess_type(attachment_path)
         mime_type, mime_subtype = mime_type.split('/', 1)
 
-        with open(attachment_path, 'rb') as ap:
-            message.add_attachment(ap.read(),
+        with open(attachment_path, 'rb') as fp:
+            message.add_attachment(fp.read(),
                                    maintype=mime_type,
                                    subtype=mime_subtype,
                                    filename=attachment_filename)
