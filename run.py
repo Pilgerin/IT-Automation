a7d55e78c0c4cd6346ada5cfc1e9a74f2db8f4bf
Pilgerin <Sobaka11>
2022-01-28 11:01:35 -0800

    Converts text files with descriptions into a dict, linking corresponding images as a part of dictionary; posts them to a server, creating catalog with description and images

--- run.py
+++ run.py
@@ -4,26 +4,26 @@ import requests
 import json
 
 
-def catalog_creation(filename, description_url):
-    for fdbck in path_in:
+def catalog_creation(localpath, description_url):
+    descriptions = os.listdir(localpath)
+    for file in descriptions:
         # traverses each file in the directory
-        fields = ['name', 'weight', 'description', 'image name']
-        if not fdbck.startswith('.'):
-            with open("/supplier-data/descriptions" + fdbck) as fb:
+        if file.endswith('.txt'):
+            with open(localpath + file) as fb:
+                fruit = os.path.splitext(file)[0]
                 description = fb.read().split('\n')
                 print(description)
                 line_dict = {'name': description[0], 'weight': int(description[1].strip('lbs')),
                              'description': description[2],
-                             'image name': os.path.basename('/supplier-data/images' + fdbck.strip('txt') + 'jpeg')}
-                r = requests.post(url, data=line_dict)
-            with open('report.txt', 'w') as convert_file:
-                convert_file.write(json.dumps(line_dict))
-    print(r.text)
-    print(r.status_code)
+                             'image name': fruit + '.jpeg'}
+                print(line_dict)
+                r = requests.post(url, json=line_dict)
+                print(r.text)
+                print(r.status_code)
     return 0
 
 
 if __name__ == '__main__':
-    path_in = os.listdir('/supplier-data/descriptions')
-    url = 'http://[linux-instance-IP-Address]/media/fruits/'
-    catalog_creation(path_in, url)
+    path = 'supplier-data/descriptions/'
+    url = 'http://localhost/fruits/'
+    catalog_creation(path, url)
