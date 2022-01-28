97ce8c1e67dbcc112c880c22f5671574b98dc761
Pilgerin <Sobaka11>
2022-01-28 10:53:00 -0800

    Posts all files with a specific extension to webserver (based on Django)

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -2,13 +2,22 @@
 import os
 import requests
 
-path_in = os.listdir('/supplier-data/images')
-url = 'http://[linux-instance-IP-Address]/media/images/'
+path = 'supplier-data/images/'
+images = os.listdir(path)
+url = 'http://localhost/upload/'
 # Django needs trailing slash!
-for img in path_in:
+for img in images:
     # traverses each file in the directory
-    if not img.startswith('.') and "jpeg" in img:
-        with open(path_in + img, 'rb') as fb:
+    if img.endswith(".jpeg"):
+        with open(path + img, 'rb') as fb:
             r = requests.post(url, files={'file': fb})
-print(r.text)
-print(r.status_code)
+            print(r.text)
+            print(r.status_code)
+
+'''#!/usr/bin/env python3
+import requests
+
+url = "http://localhost/upload/"
+with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
+    r = requests.post(url, files={'file': opened})
+'''
